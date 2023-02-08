83f791b48fc1a36a38bc6d9af0d72d2685c502cd
skyman529 <skyman529@gmail.com>
2023-02-08 00:27:49 -0700

    first time working

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,7 +33,32 @@ const Projects = () => {
         <p>A poke tracker app for keeping track of all your pokemon's across all your pokemon game's</p>
       </div>
     </a>
+    <a href="https://github.com/skyman529/PoketrackerApp"  className='container project__container'>
+      <div className='about__project'>
+        <div className='about__project-image'>
+          <img src={Project1} alt='Project1'/>
+        </div>
+        <p>A poke tracker app for keeping track of all your pokemon's across all your pokemon game's</p>
+      </div>
+    </a>
+    <a href="https://github.com/skyman529/MOVIEWISHLISTproject" className='container project__container'>
+      <div className='about__project'>
+        <div className='about__project-image'>
+          <img src={Project2} alt='Project2'/>
+        </div>
+        <p>A poke tracker app for keeping track of all your pokemon's across all your pokemon game's</p>
+      </div>
+    </a>  
+    <a href="https://github.com/skyman529/TheCut" className='container project__container'>
+      <div className='about__project'>
+        <div className='about__project-image'>
+          <img src={Project3} alt='Project3'/>
+        </div>
+        <p>A poke tracker app for keeping track of all your pokemon's across all your pokemon game's</p>
+      </div>
+    </a>
    </section>
+   
   )
 }
 
